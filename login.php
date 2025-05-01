ceb8bc6e1dec4ba22f53760e6aa1fae8b1e0696c
heelchakarani <124016810+heelchakarani@users.noreply.github.com>
2025-05-01 17:49:20 +0530

    app

--- login.php
+++ login.php
@@ -1,48 +1,72 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Login | Task Management System</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/style.css">
-</head>
-<body class="login-body">
+<?php 
+session_start();
+if (isset($_POST['user_name']) && isset($_POST['password'])) {
+	include "../DB_connection.php";
+
+    function validate_input($data) {
+	  $data = trim($data);
+	  $data = stripslashes($data);
+	  $data = htmlspecialchars($data);
+	  return $data;
+	}
+
+	$user_name = validate_input($_POST['user_name']);
+	$password = validate_input($_POST['password']);
+
+	if (empty($user_name)) {
+		$em = "User name is required";
+	    header("Location: ../login.php?error=$em");
+	    exit();
+	}else if (empty($password)) {
+		$em = "Password name is required";
+	    header("Location: ../login.php?error=$em");
+	    exit();
+	}else {
+    
+       $sql = "SELECT * FROM users WHERE username = ?";
+       $stmt = $conn->prepare($sql);
+       $stmt->execute([$user_name]);
+
+       if ($stmt->rowCount() == 1) {
+       	   $user = $stmt->fetch();
+       	   $usernameDb = $user['username'];
+       	   $passwordDb = $user['password'];
+       	   $role = $user['role'];
+       	   $id = $user['id'];
+
+       	   if ($user_name === $usernameDb) {
+	       	   	if (password_verify($password, $passwordDb)) {
+	       	   		if ($role == "admin") {
+	       	   			$_SESSION['role'] = $role;
+	       	   			$_SESSION['id'] = $id;
+	       	   			$_SESSION['username'] = $usernameDb;
+                        header("Location: ../index.php");
+	       	   		}else if ($role == 'employee') {
+	       	   			$_SESSION['role'] = $role;
+	       	   			$_SESSION['id'] = $id;
+	       	   			$_SESSION['username'] = $usernameDb;
+                        header("Location: ../index.php");
+	       	   		}else {
+	       	   		   $em = "Unknown error occurred ";
+							   header("Location: ../login.php?error=$em");
+							   exit();
+	       	   		}
+	       	   	}else {
+	       	   	   $em = "Incorrect username or password ";
+						   header("Location: ../login.php?error=$em");
+						   exit();
+	       	   }
+       	   }else {
+       	   	   $em = "Incorrect username or password ";
+			   header("Location: ../login.php?error=$em");
+			   exit();
+       	   }
+       }
       
-      <form method="POST" action="app/login.php" class="shadow p-4">
 
-      	  <h3 class="display-4">LOGIN</h3>
-      	  <?php if (isset($_GET['error'])) {?>
-      	  	<div class="alert alert-danger" role="alert">
-			  <?php echo stripcslashes($_GET['error']); ?>
-			</div>
-      	  <?php } ?>
-
-      	  <?php if (isset($_GET['success'])) {?>
-      	  	<div class="alert alert-success" role="alert">
-			  <?php echo stripcslashes($_GET['success']); ?>
-			</div>
-      	  <?php } 
-
-                // $pass = "123";
-                // $pass = password_hash($pass, PASSWORD_DEFAULT);
-                // echo $pass;
-      
-      	  ?>
-  
-			
-		  <div class="mb-3">
-		    <label for="exampleInputEmail1" class="form-label">User name</label>
-		    <input type="text" class="form-control" name="user_name">
-		  </div>
-		  <div class="mb-3">
-		    <label for="exampleInputPassword1" class="form-label">Password</label>
-		    <input type="password" class="form-control" name="password" id="exampleInputPassword1">
-		  </div>
-		  <button type="submit" class="btn btn-primary">Login</button>
-		</form>
-
-
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+	}
+}else {
+   $em = "Unknown error occurred";
+   header("Location: ../login.php?error=$em");
+   exit();
+}
